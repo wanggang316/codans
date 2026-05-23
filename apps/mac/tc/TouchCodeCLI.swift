d3d3ed6f22a1b3d6cc557b098ac2dd44c144cc43
Gump <1989wg@gmail.com>
2026-05-23 18:50:20 +0800

    chore(release): bump to 0.2.5

--- apps/mac/tc/TouchCodeCLI.swift
+++ apps/mac/tc/TouchCodeCLI.swift
@@ -7,7 +7,7 @@ import tcKit
 
 @main
 struct TouchCodeCLI: AsyncParsableCommand {
-  static let version = "0.2.4"
+  static let version = "0.2.5"
 
   static let configuration = CommandConfiguration(
     commandName: "tc",
